a96193e2f6821dfd6de6dee2bcc5a05340a5624b
dvbtham <dvbtham@gmail.com>
2016-06-17 20:49:40 +0700

    Updated

---
+++ itforum_teamwork/Hubs/CounterHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace itforum_teamwork.Hubs
+{
+    public class CounterHub : Hub
+    {
+        static long counter = 0;
+        public override Task OnConnected()
+        {
+            counter += 1;
+            Clients.All.UpdateCount(counter);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            counter -= 1;
+            Clients.All.UpdateCount(counter);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ itforum_teamwork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(itforum_teamwork.Startup))]
+namespace itforum_teamwork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}

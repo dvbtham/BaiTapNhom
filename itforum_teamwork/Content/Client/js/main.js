18b80db9e35c3169671832a36fc7084f4a7cf893
dvbtham <dvbtham@gmail.com>
2016-06-21 20:33:01 +0700

    Updated

--- itforum_teamwork/Content/Client/js/main.js
+++ itforum_teamwork/Content/Client/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 	        scrollSpeed: 300, // Speed back to top (ms)
 	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
 	        animation: 'fade', // Fade, slide, none
-	        animationSpeed: 200, // Animation in speed (ms)
+	        animationSpeed: 150, // Animation in speed (ms)
 	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
 					//scrollTarget: false, // Set a custom target element for scrolling to the top
 	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
